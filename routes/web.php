4ff3c3cd585f4a07feabacc0ed93449c9fa2fcf9
Phat <phatt6898@gmail.com>
2019-05-08 14:53:28 +0700

    addfile

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//tao group route customers
+Route::group(['prefix' => 'customers'], function () {
+    Route::get('/','CustomerController@index')->name('customers.index');
+    Route::get('/create','CustomerController@create')->name('customers.create');
+    Route::post('/create','CustomerController@store')->name('customers.store');
+    Route::get('/{id}/edit','CustomerController@edit')->name('customers.edit');
+    Route::post('/{id}/edit','CustomerController@update')->name('customers.update');
+    Route::get('/{id}/destroy','CustomerController@destroy')->name('customers.destroy');
+});
